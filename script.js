9ba4fdea63df886ff2acaa822f682bf0e2960679
swos <szymon.wos1319@gmail.com>
2024-01-11 13:31:25 +0100

    Check if inputs are valid

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const NAME_PATTERN = "^[a-z ,.'-]+$";
 const EMAIL_PATTERN = "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$";
-const PHONE_PATTERN = "(\+[0-9]{10}[0-9]?)|([0-9]{9})";
+const PHONE_PATTERN = "^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$";
 const PASSWORD_PATTERN = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$";
 
 const firstNameInput = document.querySelector('#first-name');
@@ -9,3 +9,41 @@ const emailInput = document.querySelector('#email');
 const phoneInput = document.querySelector('#phone');
 const password = document.querySelector('#password');
 const confirmedPassword = document.querySelector('#confirmed-password');
+const form = document.querySelector('form');
+
+const inputs = [];
+
+class dataInput {
+    constructor(input, pattern){
+        this.input = input;
+        this.pattern = pattern;
+    }
+}
+
+function setArray(){
+    inputs.push(new dataInput(firstNameInput, NAME_PATTERN));
+    inputs.push(new dataInput(lastNameInput, NAME_PATTERN));
+    inputs.push(new dataInput(emailInput, EMAIL_PATTERN));
+    inputs.push(new dataInput(phoneInput, PHONE_PATTERN));
+    inputs.push(new dataInput(password, PASSWORD_PATTERN));
+    inputs.push(new dataInput(confirmedPassword, PASSWORD_PATTERN));
+}
+
+function checkWithPattern(inputValue, pattern){
+    return new RegExp(pattern).test(inputValue);
+}
+
+function validateInputs(){
+    let isValid = true;
+    for(let i=0; i<inputs.length; i++){
+        if(!checkWithPattern(inputs[i].input.value, inputs[i].pattern)){
+            isValid = false;
+        }
+    }
+}
+
+setArray();
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    validateInputs();
+})
